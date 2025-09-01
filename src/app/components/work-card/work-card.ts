45a56cd693ed52ffa188bc75319366815d7c7e73
maugalcst <maugal.cst@gmail.com>
2025-08-31 19:06:10 -0600

    added display logic in work card component

--- src/app/components/work-card/work-card.ts
+++ src/app/components/work-card/work-card.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-work-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './work-card.html',
   styleUrl: './work-card.css'
 })
 export class WorkCard {
-
+  @Input() companyName: string = '';
+  @Input() companyDuration: string = '';
+  @Input() jobPosition: string = '';
+  @Input() jobDescription: string = '';
+  @Input() technologies: string[] = [];
 }
