50875433a06a7ea8910f28b1bf7b88da8d82df28
maugalcst <maugal.cst@gmail.com>
2025-08-31 21:28:45 -0600

    styling of video background for work experience

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,36 @@
 import { Component, signal, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { WorkCard } from './components/work-card/work-card';
 
+interface Job {
+  companyName: string;
+  companyDuration: string;
+  jobPosition: string;
+  jobDescription: string;
+  technologies?: string[];
+}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WorkCard],
+  imports: [WorkCard],
   templateUrl: './app.html',
   styleUrl: './app.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class App {
   protected readonly title = signal('mi-portfolio');
+
+  workExperience: Job[] = [
+    {
+      companyName: 'Epicor',
+      companyDuration: 'Jun 2025 - Present',
+      jobPosition: 'QA Developer Intern',
+      jobDescription: 'As a QA Developer Intern at Epicor, I specialize in test automation using WebdriverIO. My focus is on building and maintaining robust testing frameworks to ensure high-quality, reliable software.',
+      technologies: ['WebdriverIO', 'Jiraa', 'Git', 'Zephyr', 'Azure DevOps']
+    },
+    {
+      companyName: 'Banorte',
+      companyDuration: 'Jun 2024 - Oct 2024',
+      jobPosition: 'Customer Support Agent',
+      jobDescription: 'I assisted clients with a wide range of inquiries, including mobile app assistance, investments, and fraud reports. My role involved providing effective solutions for a high volume of cases, which honed my skills in communication.'
+    }
+  ];
 }
